eb12569fe23072ad1b4e3928582f5140dd4aea37
MaxJara <maxmata77@gmail.com>
2021-07-04 18:26:08 -0500

    Se aplico polimorfismo

--- src/Main.java
+++ src/Main.java
@@ -12,9 +12,14 @@ public class Main {
         myDoctor.addAvailableAppointment(new Date(), "10am");
         myDoctor.addAvailableAppointment(new Date(), "1pm");
 
+        System.out.println(myDoctor);
+
+        /*
         for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
             System.out.println(aA.getDate() + ""+ aA.getTime());
         }
+
+         */
 
         System.out.println();
         System.out.println();
