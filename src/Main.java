1962ae7c5fcc10426835b371950a063bfb6a5bf4
MaxJara <maxmata77@gmail.com>
2021-07-04 16:24:31 -0500

    Se aplico getters y setters

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,13 @@ public class Main {
         System.out.println(myDoctor.name);
         System.out.println(myDoctor.speciality);
 
+        Patient patient = new Patient("Alejandra", "alejandra@mail.com");
+        patient.setWeight(54.6);
+        System.out.println(patient.getWeight());
+
+        patient.setPhoneNumber("123456789");
+        System.out.println(patient.getPhoneNumber());
+
+
     }
 }
