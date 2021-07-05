97588b8a88daeeca783ed2453f78cef8ddd6af9b
MaxJara <maxmata77@gmail.com>
2021-07-04 21:42:31 -0500

    Se agrego el menu del paciente

--- src/Main.java
+++ src/Main.java
@@ -1,61 +1,8 @@
-import model.Doctor;
-import model.ISchedulable;
-import model.Patient;
-import model.User;
-
-import java.util.Date;
+import static ui.UIMenu.*;
 
 public class Main {
     public static void main(String[] args) {
-
-        //showMenu();
-
-        Doctor myDoctor = new Doctor("Max Jara", "Pediatra");
-        myDoctor.addAvailableAppointment(new Date(), "4pm");
-        myDoctor.addAvailableAppointment(new Date(), "10am");
-        myDoctor.addAvailableAppointment(new Date(), "1pm");
-
-        System.out.println(myDoctor);
-
-        User user = new Doctor("Anahi", "ana@ana.com");
-        user.showDataUser();
-
-        User userPa = new Patient("Anahi", "ana@ana.com");
-        userPa.showDataUser();
-
-        User user1 = new User("Max", "maxmata77@gmail.com") {
-            @Override
-            public void showDataUser() {
-                System.out.println("Doctor");
-                System.out.println("Hospital: Cruz Verde");
-                System.out.println("Departamento: Geriatria");
-            }
-        };
-
-        user1.showDataUser();
-
-        ISchedulable iSchedulable = new ISchedulable() {
-            @Override
-            public void schedule(Date date, String time) {
-
-            }
-        };
-
-
-        /*
-        for (model.Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
-            System.out.println(aA.getDate() + ""+ aA.getTime());
-        }
-
-         */
-
-        /*
-        System.out.println();
-        System.out.println();
-        Patient patient = new Patient("Alejandra", "alejandra@mail.com");
-        System.out.println(patient);
-
-
-         */
+        showMenu();
     }
+
 }
