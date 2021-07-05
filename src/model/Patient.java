2d485f7125b19a32ef1620efb6161525975934e1
MaxJara <maxmata77@gmail.com>
2021-07-04 22:19:51 -0500

    Se finalizo el programa

--- src/model/Patient.java
+++ src/model/Patient.java
@@ -1,4 +1,8 @@
 package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Patient extends User {
     //Atributos
     private String birthday;
@@ -6,8 +10,30 @@ public class Patient extends User {
     private double height;
     private String blood;
 
+    private ArrayList<AppointmentDoctor> appointmentDoctors = new ArrayList<>();
+    private ArrayList<AppointmentNurse> appointmentNurses = new ArrayList<>();
+
+    public ArrayList<AppointmentDoctor> getAppointmentDoctors() {
+        return appointmentDoctors;
+    }
+
+    public void addAppointmentDoctors(Doctor doctor, Date date, String time) {
+        AppointmentDoctor appointmentDoctor = new AppointmentDoctor(this, doctor);
+        appointmentDoctor.schedule(date,time);
+        appointmentDoctors.add(appointmentDoctor);
+    }
+
+    public ArrayList<AppointmentNurse> getAppointmentNurses() {
+        return appointmentNurses;
+    }
+
+    public void setAppointmentNurses(ArrayList<AppointmentNurse> appointmentNurses) {
+        this.appointmentNurses = appointmentNurses;
+    }
+
     public Patient(String name, String email){
         super(name,email);
+        //mas instrucciones
     }
 
     // 54.5
@@ -51,8 +77,10 @@ public class Patient extends User {
     }
 
     @Override
-    public void showDataUser(){
+    public void showDataUser() {
         System.out.println("Paciente");
-        System.out.println("Historial completo desde nacimiento");
+        System.out.println("Historial completo desde naciemiento");
+
     }
+
 }
